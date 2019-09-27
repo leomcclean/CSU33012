363c80b3649565763a1d806b1bf56df25e0c58ba
leomcclean <47148527+leomcclean@users.noreply.github.com>
2019-09-27 15:51:06 +0100

    final version
    everything works, tested and all. some code isn't 'clean' as such, but will do fine. code coverage is as complete as can be.

--- LowestCommonAncestor/src/pack/lca.java
+++ LowestCommonAncestor/src/pack/lca.java
@@ -1,98 +1,112 @@
 package pack;
 
+import java.util.Arrays;
+
 public class lca
 {
     public Node root;
 
     public class Node
     {
-        private int key;
         private int value;
-        private Node left, right;
-        private int N;
+        public Node left, right;
 
-        public Node(int key, int value, int N)
+        public Node(int value)
         {
-            this.key = key;
             this.value = value;
-            this.N = N;
         }
     }
     
-    // return number of int-value pairs in BST
-    public int size()
-    {
-    	return size(root);
-    }
-
-    // return number of int-value pairs in BST rooted at x
-    private int size(Node x)
-    {
-        if (x == null)
-        	return 0;
-        else
-        	return x.N;
-    }
-    
     // recursively navigates a tree to create a lineage
-    private int[] listAncestors(Node y)
+    private int[] listAncestors(int y)
     {
-    	String roughLineage = listAncestorsRecursive(root, y, "") + "." + root;
+    	String roughLineage = listAncestorsRecursive(root, y, "") + ".";
     	return sire(roughLineage);
-    }
-    
-    private String listAncestorsRecursive(Node x, Node y, String answer)
-    {
-    	if(x.left != null && answer == "")
-    	{
-    		if(x.left != y)
-    			answer = listAncestorsRecursive(x.left, y, answer);
-    		else
-    			return answer + "." + x.left.value;
-    	}
-    	if(x.right != null && answer == "")
-    	{
-    		if(x.right != y)
-    			answer = listAncestorsRecursive(x.right, y, answer);
-    		else
-    			return answer + "." + x.right.value;
-    	}
-    	if(answer == "")
-    		return answer;
-    	else
-    		return answer + "." + x.value;
     }
     
     // turns a delimited lineage String into a lineage Array
     private int[] sire(String roughLineage)
     {
-    	int stringlength = roughLineage.length();
-    	int lineage[] = new int[stringlength / 2];
-    	for(int i = 1, j = 0; i <= stringlength; i += 2, j++)
+    	// this is all 'bad' code, using an ArrayList or something similar was the correct solution
+    	int stringLength = roughLineage.length();
+    	int lineage[] = new int[stringLength / 2];
+    	int ancestor = 0;
+    	for(int i = 1, j = 0, k = 0; i < stringLength; i++)
     	{
-    		lineage[j] = roughLineage.charAt(i);
+    		// convert the multi-digit numbers nicely from chars to a single int
+    		char extract = roughLineage.charAt(i);
+    		if(extract != '.')
+    		{
+    			if(k >= 1)
+    				ancestor = ancestor * 10 + roughLineage.charAt(i) - 48;
+    			else
+    				ancestor = roughLineage.charAt(i) - 48;
+    			k++;
+    		}else
+    		{
+    			lineage[j] = ancestor;
+    			j++;
+    			k = 0;
+    		}
+    	}
+    	// fix array sizes because they are initialised to silly sizes
+    	for(int i = 0; i < lineage.length; i++)
+    	{
+    		if(lineage[i] == 0)
+    		{
+    			lineage = Arrays.copyOf(lineage, i);
+    			break;
+    		}
     	}
     	return lineage;
     }
     
+    private String listAncestorsRecursive(Node x, int y, String answer)
+    {
+    	if(x.left != null && answer == "")
+    	{
+    		if(x.left.value != y)
+    			answer = listAncestorsRecursive(x.left, y, answer);
+    		else
+    			//have to also print the current value to properly list it
+    			return answer + "." + x.left.value + "." + x.value;
+    	}
+    	if(x.right != null && answer == "")
+    	{
+    		if(x.right.value != y)
+    			answer = listAncestorsRecursive(x.right, y, answer);
+    		else
+    			return answer + "." + x.right.value + "." + x.value;
+    	}
+    	if(answer != "")
+    		return answer + "." + x.value;
+    	else
+    		return answer;
+    }
+    
     // find the lowest common ancestor by comparing lineages
-    public int lowestCommonAncestor(Node list[])
+    public int lowestCommonAncestor(int list[])
     {
     	int lineages[][] = new int[list.length][];
     	for(int i = 0; i < list.length; i++)
     	{
     		lineages[i] = listAncestors(list[i]);
     	}
-    	
-    	int shortest = 0;
+    	// finds the shortest array so we never go beyond its length
+    	int shortCount = 0;
+    	int shortName = 0;
     	for(int i = 0; i < lineages.length; i++)
     	{
-    		if(lineages[i][0] < shortest || shortest == 0)
-    			shortest = lineages[0][i];
+    		if(lineages[i][0] < shortCount || shortCount == 0)
+    		{
+    			shortName = i;
+    			shortCount = lineages[0][i];
+    		}
     	}
-    	
+    	// iterate through, taking each value in our first array and checking to see its presence in the others
+    	// since the arrays are in descending value, the lca is whatever we find first
     	int lca = 0;
-    	for(int i = 1; i < shortest; i++)
+    	for(int i = 1; i < shortCount; i++)
     	{
     		boolean test1 = true;
     		for(int j = 1; j < lineages.length; j++)
@@ -100,8 +114,11 @@ public class lca
     			boolean test2 = false;
     			for(int k = 0; k < lineages[j].length; k++)
     			{
-    				if(lineages[j][k] == lineages[0][i])
+    				if(lineages[j][k] == lineages[shortName][i])
+    				{
     					test2 = true;
+    					break;
+    				}
     			}
     			if(!test2)
     			{
@@ -111,30 +128,10 @@ public class lca
     		}
     		if(test1)
     		{
-    			lca = lineages[0][i];
+    			lca = lineages[shortName][i];
     			break;
     		}
     	}
     	return lca;
-    }
-    
-    // delete these later leo
-    public String printKeys()
-    {
-    	return printKeys(root, "");
-    }
-    
-    private String printKeys(Node current, String prefix)
-    {
-    	String result = "";
-        if(current == null)
-        {
-        	result += prefix + "-null\n";
-        	return result;
-        }
-        result += prefix + "-" + current.key + "\n";
-        result += printKeys(current.left, prefix + " |");
-        result += printKeys(current.right, prefix + "  ");
-    	return result;
     }
 }
--- LowestCommonAncestor/src/pack/testing.java
+++ LowestCommonAncestor/src/pack/testing.java
@@ -1,16 +1,75 @@
 package pack;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.Assert.assertEquals;
 import org.junit.jupiter.api.Test;
 
 class testing
 {
-
 	@Test
-	void test()
+	void testSix_RootLCA()
 	{
-		fail("Not yet implemented");
+		lca tree = new lca();
+		tree.root = tree.new Node(1);
+		tree.root.right = tree.new Node(2);
+		tree.root.right.right = tree.new Node(3);
+		tree.root.right.right.left = tree.new Node(4);
+		tree.root.left = tree.new Node(5);
+		tree.root.left.right = tree.new Node(6);
+		int[] array = {4,6};
+		assertEquals(tree.lowestCommonAncestor(array), 1);
 	}
 
+	@Test
+	void testSix_InLCA()
+	{
+		lca tree = new lca();
+		tree.root = tree.new Node(1);
+		tree.root.right = tree.new Node(2);
+		tree.root.right.right = tree.new Node(3);
+		tree.root.left = tree.new Node(5);
+		tree.root.left.left = tree.new Node(4);
+		tree.root.left.right = tree.new Node(6);
+		int[] array = {4,6};
+		assertEquals(tree.lowestCommonAncestor(array), 5);
+	}
+
+	@Test
+	void testTwelve_RootLCA()
+	{
+		lca tree = new lca();
+		tree.root = tree.new Node(1);
+		tree.root.right = tree.new Node(2);
+		tree.root.right.right = tree.new Node(3);
+		tree.root.right.right.left = tree.new Node(4);
+		tree.root.left = tree.new Node(5);
+		tree.root.left.right = tree.new Node(6);
+		tree.root.right.left = tree.new Node(7);
+		tree.root.right.right.left.right = tree.new Node(8);
+		tree.root.right.right.left.right.right = tree.new Node(9);
+		tree.root.left.right.left = tree.new Node(10);
+		tree.root.left.right.left.right = tree.new Node(11);
+		tree.root.left.right.left.right.left = tree.new Node(12);
+		int[] array = {2,3,11,12};
+		assertEquals(tree.lowestCommonAncestor(array), 1);
+	}
+	
+	@Test
+	void testTwelve_InLCA()
+	{
+		lca tree = new lca();
+		tree.root = tree.new Node(1);
+		tree.root.right = tree.new Node(2);
+		tree.root.right.right = tree.new Node(3);
+		tree.root.right.right.left = tree.new Node(4);
+		tree.root.left = tree.new Node(5);
+		tree.root.left.right = tree.new Node(6);
+		tree.root.right.left = tree.new Node(7);
+		tree.root.right.right.left.right = tree.new Node(8);
+		tree.root.right.right.left.left = tree.new Node(9);
+		tree.root.left.right.left = tree.new Node(10);
+		tree.root.left.right.left.right = tree.new Node(11);
+		tree.root.left.right.left.right.left = tree.new Node(12);
+		int[] array = {8,9,7};
+		assertEquals(tree.lowestCommonAncestor(array), 2);
+	}
 }
