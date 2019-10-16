33168b2268cbf95c1b916b9d71b1517609b62f8b
leomcclean <47148527+leomcclean@users.noreply.github.com>
2019-10-16 16:04:43 +0100

    completed algorithm
    used an advised algorithm that should return a list of  lowest common ancestors, than returns the first one of the list.

--- LowestCommonAncestor/src/pack/dag.java
+++ LowestCommonAncestor/src/pack/dag.java
@@ -30,7 +30,7 @@ public class dag
 	}
 	
 	// test for cycles within a graph to ensure it is a DAG
-	public boolean isAcyclic(ArrayList test)
+	public boolean isAcyclic(ArrayList<ArrayList<Integer>> test)
 	{
 		// a graph with one node or zero nodes is acyclic
 		if(test.size() == 1 || test.isEmpty())
@@ -56,4 +56,57 @@ public class dag
 		}
 		return true;
 	}
+
+	public int lca(int one, int two)
+	{
+		int[] visited = new int[size];
+		int[] familyOne = breadthFirstSearch(visited, one, 1);
+		int[] familyTwo = breadthFirstSearch(familyOne, two, 2);
+		
+		int[] count = new int[size];
+		for(int i = 0; i < size; i++)
+		{
+			if(familyTwo[i] == 2)
+			{
+				ArrayList<Integer> temp = adj.get(i);
+				for(int j = 0; j < temp.size(); j++)
+				{
+					if(familyTwo[temp.get(j)] == 2)
+						count[i]++;
+				}
+			}
+		}
+		for(int i = 0; i < count.length; i++)
+		{
+			if(familyTwo[i] == 2 && count[i] == 0)
+				return count[i];
+		}
+		return -1;
+	}
+	
+	private int[] breadthFirstSearch(int[] visited, int start, int base)
+	{
+		ArrayList<Integer> queue = new ArrayList<Integer>(); 
+
+		visited[start] = 1; 
+		queue.add(start); 
+  
+		while(queue.size() != 0) 
+		{ 
+			start = queue.get(0);
+			queue.remove(0);
+  
+			for(int i = 0; i < adj.get(start).size(); i++) 
+			{ 
+				int j = adj.get(start).get(i);
+				if (visited[j] == (base - 1)) 
+				{ 
+					visited[j] = base; 
+					queue.add(j);
+				}
+			}
+		}
+		return visited;
+	}
+	
 }
