340156cebf2fea0dd50f8e26a85c875bf720da34
leomcclean <47148527+leomcclean@users.noreply.github.com>
2019-10-17 16:17:24 +0100

    final working solution
    code coverage and tests indicate that this can find the lowest common ancestor

--- LowestCommonAncestor/src/pack/dag.java
+++ LowestCommonAncestor/src/pack/dag.java
@@ -5,6 +5,7 @@ public class dag
 {
 	private int size;
 	private ArrayList<ArrayList<Integer>> adj;
+	private ArrayList<ArrayList<Integer>> radj;
 	
 	// set the adjacency list to the size of the number of nodes
 	public dag(int size)
@@ -12,9 +13,12 @@ public class dag
 		this.size = size;
 		if(size > 0)
 		{
-			adj = new ArrayList<ArrayList<Integer>>(size);
+			adj = new ArrayList<ArrayList<Integer>>();
 			for(int i = 0; i < size; i++)
 				adj.add(new ArrayList<Integer>());
+			radj = new ArrayList<ArrayList<Integer>>();
+			for(int i = 0; i < size; i++)
+				radj.add(new ArrayList<Integer>());
 		}
 	}
 	
@@ -22,47 +26,11 @@ public class dag
 	public boolean edge(int start, int end)
 	{
 		// if any of the variables are invalid
-		if(start > size || start < 0 || end > size || end < 0)
+		if(start > size - 1	 || start < 0 || end > size - 1 || end < 0)
 			return false;
 		// add the edge
 		adj.get(start).add(end);
-		if(isAcyclic(adj))
-		{
-			// if the addition creates a cycle we remove the new edge and return an error
-			adj.get(start).remove(adj.get(start).size() - 1);
-			return false;
-		}else
-		{
-			// if the addition does not create a cycle we can add it to the main adjacency list
-			return true;
-		}
-	}
-	
-	// test for cycles within a graph to ensure it is a DAG
-	private boolean isAcyclic(ArrayList<ArrayList<Integer>> test)
-	{
-		// a graph with one node or zero nodes is acyclic
-		if(test.size() == 1 || test.isEmpty())
-			return true;
-		// a graph must have one leaf to be acyclic
-		// if we peel off all leaves and are left with nothing, the graph is acyclic
-		ArrayList<ArrayList<Integer>> adj2 = test;
-		while(adj2.isEmpty() == false)
-		{
-			int j = -1;
-			for(int i = 0; i < adj2.size(); i++)
-			{
-				if(adj2.get(i).isEmpty())
-				{
-					j = i;
-					break;
-				}
-			}
-			if(j == -1)
-				return false;
-			else
-				adj2.remove(j);
-		}
+		radj.get(end).add(start);
 		return true;
 	}
 
@@ -77,29 +45,13 @@ public class dag
 		int[] familyOne = breadthFirstSearch(visited, one, 1);
 		int[] familyTwo = breadthFirstSearch(familyOne, two, 2);
 		
-		// derive the lca by finding the closest ancestor
-		int[] count = new int[size];
+		//we derive the lca by finding the closest ancestor
+		// the lowest score (0) is an ancestor with no children closer to the two nodes
 		for(int i = 0; i < size; i++)
 		{
-			// if a node is an ancestor of the pair...
-			if(familyTwo[i] == 2)
-			{
-				// ...see if it has a parent that is also an ancestor...
-				ArrayList<Integer> temp = adj.get(i);
-				for(int j = 0; j < temp.size(); j++)
-				{
-					// ...if it does, increment the parents score
-					if(familyTwo[temp.get(j)] == 2)
-						count[j]++;
-				}
-			}
-		}
-		// the lowest score (0) is an ancestor with no children closer to the two nodes
-		for(int i = 0; i < count.length; i++)
-		{
 			// therefore any node with a (0) score is the lca
-			if(familyTwo[i] == 2 && count[i] == 0)
-				return count[i];
+			if(familyTwo[i] == 2)
+				return i;
 		}
 		// if there are no ancestors in common we can return (-1) for a failure
 		return -1;
@@ -109,20 +61,19 @@ public class dag
 	{
 		ArrayList<Integer> queue = new ArrayList<Integer>(); 
 
-		visited[start] = 1; 
 		queue.add(start); 
   
 		while(queue.size() != 0) 
 		{ 
-			start = queue.get(0);
+			int current = queue.get(0);
 			queue.remove(0);
-  
-			for(int i = 0; i < adj.get(start).size(); i++) 
-			{ 
-				int j = adj.get(start).get(i);
-				if (visited[j] == (base - 1)) 
+			
+			if(!radj.get(current).isEmpty())
+			{
+				for(int i = 0; i < radj.get(current).size(); i++) 
 				{ 
-					visited[j] = base; 
+					int j = radj.get(current).get(i);
+					visited[j]++; 
 					queue.add(j);
 				}
 			}
--- LowestCommonAncestor/src/pack/testing.java
+++ LowestCommonAncestor/src/pack/testing.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 class testing
 {
+
 	@Test
 	void badDAG()
 	{
@@ -19,17 +20,39 @@ class testing
 		dag test = new dag(2);
 		assertFalse(test.edge(-1, 1) && test.edge(0, 3) && test.edge(0, -2) && test.edge(4, 1));
 	}
-	
+
 	@Test
-	void testCyclic()
+	void testLCA3()
 	{
 		dag test = new dag(3);
-		assertFalse(!test.edge(0, 1) && !test.edge(1, 2) && test.edge(2, 3));
+		test.edge(0, 1);
+		test.edge(0, 2);
+		assertEquals(test.lca(1, 2), 0);
 	}
 
 	@Test
-	void testTwelveNodeLCA()
+	void testLCA1()
 	{
+		dag test = new dag(1);
+		test.edge(0, 1);
+		assertEquals(test.lca(0, 0), -1);
+	}
+	
+	@Test
+	void testLCA0()
+	{
+		dag test = new dag(0);
+		assertEquals(test.lca(0, 0), -1);
+	}
 
+	@Test
+	void testLCA5()
+	{
+		dag test = new dag(5);
+		test.edge(0, 1);
+		test.edge(0, 2);
+		test.edge(1, 4);
+		test.edge(2, 3);
+		assertEquals(test.lca(3, 4), 0);
 	}
 }
